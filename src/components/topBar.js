f0c9d76affdfd9b77768f53009b51393c6dd8599
Muath <muath.khalifa@yahoo.com>
2022-10-11 04:16:42 +0100

    first commit

---
+++ src/components/topBar.js
@@ -0,0 +1,17 @@
+import react from "react"
+
+
+const TopBar = () => {
+    
+    return(
+        <div>
+           <div className="top-div">
+                <button>Pomodor</button>
+                <button>Short Break</button>
+                <button>Long Long Break</button>
+           </div>
+        </div>
+    )
+}
+
+export default TopBar;
